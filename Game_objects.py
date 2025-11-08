953d77d2110c45f9e2cafd284846abfda8b6b333
Luke Ferley <lukeferley6@gmail.com>
2025-11-08 14:21:02 +0200

    clean and refactor

--- Game_objects.py
+++ Game_objects.py
@@ -3,6 +3,7 @@ import random
 import os
 
 class Player(pygame.sprite.Sprite):
+
     def __init__(self, x, y, screen_width, screen_height):
         super().__init__()
         self.x, self.y = x, y
@@ -12,8 +13,6 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.transform.scale(self.image, (self.w,self.h))
         self.rect = self.image.get_rect(center = (x, y))
         self.speed = 10
-
-
         
     def key_logic(self):
         keys = pygame.key.get_pressed()
@@ -26,15 +25,15 @@ class Player(pygame.sprite.Sprite):
         if keys[pygame.K_s] and self.rect.y < self.screen_h - self.h - self.speed:
             self.rect.y += self.speed
 
-
     def update(self):
         self.key_logic()
 
 class Food(pygame.sprite.Sprite):
+
     MAX_FALL_RATE = 35
     fall_rate = 2.5
 
-    def __init__(self, screen_width: int, screen_height:int, collision_status: bool):
+    def __init__(self, screen_width: int, screen_height:int, collision_status: bool = False):
         super().__init__()
         self.screen_w, self.screen_h = screen_width, screen_height
         self.x, self.y = random.uniform(80.0, float(screen_width - 80)), 0
@@ -53,17 +52,16 @@ class Food(pygame.sprite.Sprite):
             self.rect.x = random.uniform(80.0, float(self.screen_w - 80))         
             Food.fall_rate = min(Food.fall_rate * 1.1, Food.MAX_FALL_RATE)
 
-    
-            
     def update(self):
         self.fall()
 
 
 class Enemy(pygame.sprite.Sprite):
+    
     fall_rate = 2
     MAX_FALL_RATE = 35
 
-    def __init__(self, screen_width, screen_height, collision_status):
+    def __init__(self, screen_width, screen_height, collision_status: bool = False):
         super().__init__()
         self.screen_w, self.screen_h = screen_width, screen_height
         self.x, self.y = random.randint(80, screen_width - 80), 0
@@ -71,7 +69,6 @@ class Enemy(pygame.sprite.Sprite):
         self.image = pygame.image.load(os.path.join('images', "black_berry.png")).convert_alpha()
         self.image = pygame.transform.scale(self.image, (self.w, self.h))
         self.rect = self.image.get_rect(center = (self.x, self.y))
-        # self.fall_rate = 2
 
     def fall(self):
         if self.rect.y < self.screen_h:
@@ -82,7 +79,6 @@ class Enemy(pygame.sprite.Sprite):
             self.rect.x = random.randint(self.screen_w // 2, self.screen_w - 80)
             Enemy.fall_rate = min(Enemy.fall_rate * 1.1, Enemy.MAX_FALL_RATE)
     
-                
     def update(self):
         self.fall()
 
--- first-game.py
+++ first-game.py
@@ -3,70 +3,87 @@ import os
 from Game_objects import Player, Food, Enemy
 import time
 
+#initialize pygame
 pygame.init()
 pygame.font.init()
 
+# create window 
 W, H = 900, 650
 screen = pygame.display.set_mode((W, H))
 pygame.display.set_caption("First game")
 backgroung_img = pygame.image.load(os.path.join('images', 'background.jpg')).convert()
 backgroung_img = pygame.transform.scale(backgroung_img, (W, H))
-player = Player(W // 2, H // 2, W, H)
-score = 0
-collision = False
-food = Food(W, H, collision)
-enemy = Enemy(W, H, collision)
-sprite_groups = pygame.sprite.Group(player)
-food_group = pygame.sprite.Group(food) 
-enemies = pygame.sprite.Group(enemy)     
+
+# clock for fps and font init
 clock = pygame.time.Clock()
 font = pygame.font.SysFont("comic sans", 36)
-died = False
 
-pygame.time.delay(125)
-run = True
-while run:
-    # pygame.time.delay(550)
 
-    clock.tick(120)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+    
+def objects():
+
+    player = Player(W // 2, H // 2, W, H)
+    food = Food(W, H)
+    enemy = Enemy(W, H)
+    sprite_groups = pygame.sprite.Group(player)
+    food_group = pygame.sprite.Group(food) 
+    enemies = pygame.sprite.Group(enemy)     
+
+    return player, food, enemy, sprite_groups, food_group, enemies
+
+
+def game_loop(player: object, food: object , enemy: object, sprite_groups: object , food_group: object , enemies: object):
+
+    pygame.time.delay(200)
+    score = 0
+    run, died = True, False
+
+    # main game loop
+    while run:
+
+        clock.tick(120)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+        sprite_groups.update()
+        food_group.update()
+        enemies.update()
+
+        screen.blit(backgroung_img, (0,0))
+        sprite_groups.draw(screen)
+        food_group.draw(screen)
+        enemies.draw(screen)
+
+        screen_txt = font.render(f"Score: {score}",True, (255,255,255))
+        screen.blit(screen_txt, (10,10))
+        food_collision = pygame.sprite.spritecollide(player, food_group, True)
+        enemy_collision = pygame.sprite.spritecollide(enemy, sprite_groups, True)
+        
+        if food_collision:
+
+            new_food = Food(W, H)
+            new_food.fall_rate = food.fall_rate 
+            food_group.add(new_food)
+            score += 1
+
+        elif player not in sprite_groups:
+            lost = font.render(f"YOU DIED!! Final score {score}", True, (255,0,0))
+            lost_rect = lost.get_rect(center = (W // 2, H // 2))
+            screen.blit(lost, lost_rect)
+            died = True
             run = False
 
-    collision = False
-    sprite_groups.update()
-    food_group.update()
-    enemies.update()
+        pygame.display.flip()
+ 
+    pygame.time.delay(2000)   
+    pygame.quit
 
 
-    screen.blit(backgroung_img, (0,0))
-    sprite_groups.draw(screen)
-    food_group.draw(screen)
-    enemies.draw(screen)
-
-    screen_txt = font.render(f"Score: {score}",True, (255,255,255))
-    screen.blit(screen_txt, (10,10))
-    food_collision = pygame.sprite.spritecollide(player, food_group, True)
-    enemy_collision = pygame.sprite.spritecollide(enemy, sprite_groups, True)
-    
-    if food_collision:
-        collision = True # i forgot what i wanted to do with this too lazy to change it now 
-        new_food = Food(W, H, collision)
-        new_food.fall_rate = food.fall_rate 
-        food_group.add(new_food)
-        score += 1
-    elif player not in sprite_groups:
-        lost = font.render(f"YOU DIED!! Final score {score}", True, (255,0,0))
-        lost_rect = lost.get_rect(center = (W // 2, H // 2))
-        screen.blit(lost, lost_rect)
-        died = True
-        run = False
+def main():
+    player, food, enemy, sprite_groups, food_group, enemies = objects()
+    game_loop(player, food, enemy, sprite_groups, food_group, enemies)
 
 
-
-
-    pygame.display.flip()
-    # clock.tick(120)
-
-pygame.time.delay(2000)   
-pygame.quit
+if __name__ == '__main__':
+    main()
